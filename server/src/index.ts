d7a5e385a0c2744720e80c8801efd12b7da09501
Stefa <sstefandev@gmail.com>
2022-01-18 20:01:14 +0100

    first commit

---
+++ server/src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { typeDefs, resolvers } from "./graphql";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app, path: "/api" });
+
+app.listen(PORT, () => console.log(`[app]: http://localhost/${PORT}`));
